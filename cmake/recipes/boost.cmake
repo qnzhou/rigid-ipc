923c23019745449b93444cfb7a1c128a77a67a73
Qingnan Zhou <qzhou@adobe.com>
2024-07-03 16:16:28 -0400

    Update boost cmake.

--- cmake/recipes/boost.cmake
+++ cmake/recipes/boost.cmake
@@ -2,13 +2,13 @@ if(TARGET Boost::boost)
     return()
 endif()
 
-message(STATUS "Third-party: creating targets 'Boost::boost'")
+message(STATUS "Third-party: creating targets 'Boost::boost'...")
 
 include(FetchContent)
 FetchContent_Declare(
     boost-cmake
-    GIT_REPOSITORY https://github.com/Orphis/boost-cmake.git
-    GIT_TAG 7f97a08b64bd5d2e53e932ddf80c40544cf45edf
+    GIT_REPOSITORY https://github.com/libigl/boost-cmake.git
+    GIT_TAG 6bcae68ffbaaefad4583a2642ce9ea53e5e01707
 )
 
 set(PREVIOUS_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
@@ -27,3 +27,39 @@ endif()
 
 set(CMAKE_POSITION_INDEPENDENT_CODE ${OLD_CMAKE_POSITION_INDEPENDENT_CODE})
 set(CMAKE_CXX_FLAGS "${PREVIOUS_CMAKE_CXX_FLAGS}")
+
+# Set VS target folders
+set(boost_modules
+    container
+    regex
+    atomic
+    exception
+    chrono
+    wave
+    context
+    coroutine
+    date_time
+    fiber
+    filesystem
+    graph
+    iostreams
+    locale
+    log
+    log_setup
+    unit_test_framework
+    math
+    program_options
+    timer
+    random
+    serialization
+    system
+    thread
+    type_erasure
+    inteval
+)
+foreach(module IN ITEMS ${boost_modules})
+    if(TARGET Boost_${module})
+        set_target_properties(Boost_${module} PROPERTIES FOLDER ThirdParty/Boost)
+    endif()
+endforeach()
+
